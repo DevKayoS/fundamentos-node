31eacac63da74d700dd842fdeb314e5694d656d8
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-07-17 00:29:47 -0300

    feat: aprendendo sobre streams no node, aprendendo a como criar uma stream e aplicando esses conhecimentos

--- src/server.js
+++ src/server.js
@@ -4,18 +4,41 @@ import http from 'node:http'
 // POST => Criar um recurso no back-end
 // PUT => Atualizar um recurso no back-end
 // PATCH => Atualizar uma informação especifica de um recurso no back-end
+
+// Stateful - Stateless
+// Stateful salvando em memoria caso seja derrubado o servidor sera perdido todos os dados
+// Stateless - nao salva nada em memoria salva os dados externamente  
+
+// JSON -javascript object notation
+
+// cabecalhos (requisicao/resposta) => Metadados
+
+const users = [];
+
 const server = http.createServer((req, res)=> {
   const {method, url} = req
 
   if(method === 'GET' && url === '/users'){
-    return res.end('Listagem de usuários')
+    return res
+    .setHeader('Content-type', 'application/json')
+    .writeHead(200)
+    .end(JSON.stringify(users))
   }
 
+  //created user in memory
   if (method === 'POST' && url === '/users'){
-    return res.end('criacão de usuário')
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com'
+    })
+
+    return res
+      .writeHead(201) 
+      .end()
   }
 
-  return res.end('Hello world')
+  return res.writeHead(404).end('Not found')
 })
 
 
---
+++ streams/fundamentals.js
@@ -0,0 +1,45 @@
+// streams ->
+// process.stdin.pipe(process.stdout)
+
+// buffer forma de transicionar streams uma pelas outras
+
+// duplex - uniao de readble e writable
+import {Readable, Writable, Transform, Duplex} from 'node:stream'
+
+// criando uma stream do zero
+export class OneToHundredStream extends Readable {
+  index = 1
+  
+  _read(){
+    const i = this.index++
+
+   setTimeout(()=> {
+    if(i > 100){
+      this.push(null)
+    } else {
+      const buf = Buffer.from(String(i))
+      this.push(buf)
+    }
+   }, 1000)
+  }
+
+}
+
+class InverseNumberStream extends Transform{
+  _transform(chunk, enconding, callback){
+    const transformed = chunk.toString() * (-1)
+    
+    callback(null, Buffer.from(String(transformed)) )
+  }
+}
+
+class MultiplyByTenStream extends Writable {
+  _write(chunk, enconding, callback){
+    console.log(Number(chunk.toString()) * 10)
+    callback()
+  }
+}
+
+new OneToHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream())
